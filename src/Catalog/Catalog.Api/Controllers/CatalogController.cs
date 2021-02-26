3694f5033e7f555873122195bb42bf0b4833bca4
Ronny Mahlangu <ronny@trans-africa.co.za>
2021-02-26 15:44:04 +0200

    Add project files.

---
+++ src/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController: ControllerBase
+    {
+    }
+}
